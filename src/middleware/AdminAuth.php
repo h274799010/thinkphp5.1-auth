b2a498e1529d0e0ba016e66879cabdb192d6120d
h274799010 <274799010@qq.com>
2018-07-05 19:51:33 +0800

    修复中间件问题 Signed-off-by: h274799010 <274799010@qq.com>

--- src/middleware/AdminAuth.php
+++ src/middleware/AdminAuth.php
@@ -23,12 +23,12 @@ class AdminAuth
     public function handle($request, \Closure $next)
     {
         //注入用户
-        app()->auth = Auth::instance();
-        app()->user = Auth::instance()->getUserInfo();
+        app()->auth = Auth::getInstance();
+        app()->user = Auth::getInstance()->getUserInfo();
 
-        if (!Auth::instance()->notNeedLogin()) {
-            Auth::instance()->isLogin() || $this->redirect('public/login');
-            Auth::instance()->check() || $this->error('您无权限操作');
+        if (!Auth::getInstance()->notNeedLogin()) {
+            Auth::getInstance()->isLogin() || $this->redirect('public/login');
+            Auth::getInstance()->check() || $this->error('您无权限操作');
         }
 
         return $next($request);
